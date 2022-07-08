33662a0bbb57162bdd20147097384c1f5d15974b
Hai Ngo <haiqh2006ic@gmail.com>
2022-07-08 07:31:32 +0700

    Firebase 9.1.0

--- firebase.bat
+++ firebase.bat
@@ -1,5 +1,5 @@
-::call extract_package.bat com.google.external-dependency-manager 1.2.168
-call extract_firebase.bat com.google.firebase.analytics 8.7.0 firebase-analytics-unity AnalyticsDependencies.xml
-call extract_firebase.bat com.google.firebase.app 8.7.0 firebase-app-unity AppDependencies.xml
-call extract_firebase.bat com.google.firebase.crashlytics 8.7.0 firebase-crashlytics-unity CrashlyticsDependencies.xml
-call extract_firebase.bat com.google.firebase.remote-config 8.7.0 firebase-config-unity RemoteConfigDependencies.xml
+call extract_package.bat com.google.external-dependency-manager 1.2.171
+call extract_firebase.bat com.google.firebase.analytics 9.1.0 firebase-analytics-unity AnalyticsDependencies.xml
+call extract_firebase.bat com.google.firebase.app 9.1.0 firebase-app-unity AppDependencies.xml
+call extract_firebase.bat com.google.firebase.crashlytics 9.1.0 firebase-crashlytics-unity CrashlyticsDependencies.xml
+call extract_firebase.bat com.google.firebase.remote-config 9.1.0 firebase-config-unity RemoteConfigDependencies.xml
